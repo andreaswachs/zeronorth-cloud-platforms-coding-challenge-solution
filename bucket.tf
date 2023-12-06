b0abf5ce5863c3abca1a6fd43c1d6c1e0003f0b7
Andreas Wachs <andreas@wachs.dk>
2023-12-06 12:00:40 +0100

    Revert to only use suffix filter

--- bucket.tf
+++ bucket.tf
@@ -12,8 +12,7 @@ resource "aws_s3_bucket_notification" "bucket_notification" {
   lambda_function {
     lambda_function_arn = aws_lambda_function.lambda.arn
     events = ["s3:ObjectCreated:*"]
-    filter_prefix = "*"
-    filter_suffix = ".json"
+    filter_suffix = "json"
   }
 
   depends_on = [aws_lambda_permission.bucket_put_lambda_invocation]
