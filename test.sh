1b0215f1da7c5e235ad689526e7c8fca971b4f35
Andreas Wachs <andreas@wachs.dk>
2023-12-06 12:26:17 +0100

    Make test file prettier

--- test.sh
+++ test.sh
@@ -8,7 +8,7 @@ key_file="${key}.json"
 # This test script assumes that the infra+app has been deployed
 
 # Generate a JSON file with a list of random numbers
-echo "⭐️ Generating random numbers... (also deleting any existing random_numbers.json file)"
+echo "🔄 Generating random numbers... (also deleting any existing random_numbers.json file)"
 
 rm -f "${key_file}" || true
 echo -n "[" >> $key_file
@@ -28,13 +28,13 @@ do
 done
 echo -n "]" >> $key_file
 
-echo "⭐️ Expected sum: ${expected_sum}"
+echo "🔄 Expected sum: ${expected_sum}"
 
 # Upload the JSON file to S3
-echo "⭐️ Uploading random_numbers.json to S3..."
+echo "🔄 Uploading random_numbers.json to S3..."
 aws s3api put-object --bucket "${bucket}" --key "${key}.json" --body "${key_file}" > /dev/null
 
-echo -n "⭐️ Sleeping for 10 seconds to allow the lambda to process the file"
+echo -n "🔄 Sleeping for 10 seconds to allow the lambda to process the file"
 for i in {1..10}
 do
     echo -n "."
@@ -44,7 +44,7 @@ echo ""
 
 
 # Check the logs for the expected sum
-echo "⭐️ Checking the latest log stream for the expected sum..."
+echo "🔄 Checking the latest log stream for the expected sum..."
 
 log_group="/aws/lambda/cloud-coding-challenge-lambda"
 latest_log_stream=$(aws logs describe-log-streams --log-group-name $log_group | 
@@ -54,7 +54,7 @@ latest_log_stream=$(aws logs describe-log-streams --log-group-name $log_group |
     .[0]')
 latest_log_stream_name=$(echo $latest_log_stream | jq -r '.logStreamName')
 logs=$(aws logs get-log-events --log-group-name $log_group --log-stream-name $latest_log_stream_name | 
-    jq -r '.events[].message')
+    jq '.events[].message')
 
 #Prematurely clean up, since we're not sure how this script exits
 aws s3api delete-object --bucket "${bucket}" --key "${key_file}" > /dev/null
@@ -66,6 +66,8 @@ then
     exit 1
 fi
 
+# Find the exact log message that contains the expected sum
+log=$(echo $logs | jq -r ". | select(. | contains(\"Sum: ${expected_sum}\"))")
+echo "🔄 Log message: ${log}"
 echo "✅ Test passed"
-
 
