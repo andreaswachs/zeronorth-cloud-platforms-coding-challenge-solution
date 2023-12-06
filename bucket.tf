2593b510a0c9a87760ee93f7705e700120ffd547
Andreas Wachs <andreas@wachs.dk>
2023-12-06 10:07:26 +0100

    Remove uploaded test file on creation + file name filtering

--- bucket.tf
+++ bucket.tf
@@ -1,12 +1,9 @@
 resource "aws_s3_bucket" "bucket" {
   bucket = "${local.prefix}-bucket"
-}
 
-resource "aws_s3_object" "test_data_file" {
-  bucket = aws_s3_bucket.bucket.id
-  key = "test.json"
-  source = "test.json"
-  content_type = "application/json"
+  # The bucket is highly likely not to be empty, but we want to
+  # destroy it when we're done, so we force destruction
+  force_destroy = true
 }
 
 resource "aws_s3_bucket_notification" "bucket_notification" {
@@ -15,8 +12,6 @@ resource "aws_s3_bucket_notification" "bucket_notification" {
   lambda_function {
     lambda_function_arn = aws_lambda_function.lambda.arn
     events = ["s3:ObjectCreated:*"]
-    filter_prefix = "test"
-    filter_suffix = ".json"
   }
 
   depends_on = [aws_lambda_permission.bucket_put_lambda_invocation]
